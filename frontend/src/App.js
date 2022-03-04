db30b0ec010e8ba1b29770cd89e71b5205ceb7a5
prophetlin <1359241892@qq.com>
2022-03-04 12:50:31 +1100

    add data to website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import data from './data'
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">dumtreee</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Productss</h1>
+        {data.product.map((product) => (
+          <div key={product.slug}>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   )
 }
